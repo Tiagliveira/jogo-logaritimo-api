f6cf356e3cbcf4e1a89d5172b2396755c8c8961f
Tiagliveira <tiagliveira92@gmail.com>
2025-09-26 20:59:21 -0300

    � Projeto backend isolado para ranking

---
+++ api/mongo.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  id: { type: String, required: true, unique: true },
+  avatar: String,
+  nivel: { type: Number, default: 1 },
+  nivelMaximo: { type: Number, default: 1 },
+  vidas: { type: Number, default: 3 },
+  historico: { type: Array, default: [] },
+});
+
+module.exports = mongoose.models.User || mongoose.model("User", userSchema);
---
+++ api/ranking.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const User = require("./mongo");
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+module.exports = async (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "https://tiagliveira.github.io");
+  res.setHeader("Access-Control-Allow-Methods", "GET");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method !== "GET") {
+    return res.status(405).send("Método não permitido");
+  }
+
+  try {
+    const ranking = await User.find({}, "id nivelMaximo avatar")
+      .sort({ nivelMaximo: -1 })
+      .limit(10);
+    res.status(200).json(ranking);
+  } catch (err) {
+    console.error("Erro ao buscar ranking:", err.message);
+    res.status(500).send("Erro ao buscar ranking");
+  }
+};
